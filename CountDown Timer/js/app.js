cda605cba2ad40bdfe301ef526ea11cf5d6bf2c3
HAXAZE <28skofficial01@gmail.com>
2023-10-28 17:21:56 +0530

    wrote final code of javascript completed!

--- CountDown Timer/js/app.js
+++ CountDown Timer/js/app.js
@@ -1,1 +1,31 @@
-const endDate = "1 Nov 2023 10:00 PM"
+const endDate = "2 Nov 2023 10:00 PM"
+
+document.getElementById("end-date").innerText= endDate;
+const inputs = document.querySelectorAll("input")
+// const clock = ()=>{
+
+// }
+
+function clock(){
+ const end = new Date(endDate);   
+ const now = new Date();
+ const diff = (end-now)/1000;
+
+ if(diff<0) 
+ return ; 
+ inputs[0].value = Math.floor(diff/3600/24);
+ inputs[1].value = Math.floor(diff/3600)%24;
+ inputs[2].value = Math.floor(diff/60)%60;
+ inputs[3].value = Math.floor(diff)%60;
+}
+
+// initial call
+clock();
+
+setInterval(
+    ()=>{
+        clock();
+    },
+    1000
+)
+ 
