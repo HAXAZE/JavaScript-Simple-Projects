f44ec99dbcba50aa6bbf1984dca9df68e88682d0
HAXAZE <28skofficial01@gmail.com>
2023-11-11 11:44:05 +0530

    made the question array

---
+++ Quiz-App/js/app.js
@@ -0,0 +1,30 @@
+const questions = [
+    {
+        'que': 'Which of the following is a markup language?',
+        'a': 'HTML',
+        'b': 'CSS',
+        'c': 'Javascript',
+        'd': 'PHP',
+        'correct': 'a'
+    },
+    {
+        'que': 'What year was Javascript developed?',
+        'a': '1996',
+        'b': '1995',
+        'c': '1994',
+        'd': 'None of these',
+        'correct': 'b'
+    },
+    {
+        'que': 'What does CSS Stands for?',
+        'a': 'Cascading Style Sheet',
+        'b': 'Casca Style Sheet',
+        'c': 'Code Style Sheet',
+        'd': 'None of these',
+        'correct': 'a'
+    }
+]
+let index = 0;
+const loadQuestion =()=>{
+
+}
