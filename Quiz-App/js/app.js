55d6d3d4f5a4b4ea3f32788f833d0b3448036197
HAXAZE <28skofficial01@gmail.com>
2023-11-11 17:33:40 +0530

    wrote final js code logic

--- Quiz-App/js/app.js
+++ Quiz-App/js/app.js
@@ -15,7 +15,7 @@ const questions = [
         'd': 'None of these',
         'correct': 'b'
     },
-    {   
+    {
         'que': 'What does CSS Stands for?',
         'a': 'Cascading Style Sheet',
         'b': 'Casca Style Sheet',
@@ -25,11 +25,17 @@ const questions = [
     }
 ]
 let index = 0;
-const quesBox= document.getElementById("quesBox");
+let total = questions.length;
+let right = 0, wrong = 0;
+const quesBox = document.getElementById("quesBox");
 const optionInputs = document.querySelectorAll('.options');
-const loadQuestion =()=>{
+const loadQuestion = () => {
+    if (index === total) {
+        return endQuiz()
+    }
+    reset();
     const data = questions[index]
-    quesBox.innerText = `${index+1})${data.que}`;
+    quesBox.innerText = `${index + 1})${data.que}`;
     optionInputs[0].nextElementSibling.innerText = data.a;
     optionInputs[1].nextElementSibling.innerText = data.b;
     optionInputs[2].nextElementSibling.innerText = data.c;
@@ -37,8 +43,47 @@ const loadQuestion =()=>{
 
 }
 
-const SubmitQuiz =()=>{
-    
+const SubmitQuiz = () => {
+    const data = questions[index];
+    const ans = getAnswer()
+    if (ans === data.correct) {
+        right++;
+    }
+    else {
+        wrong++;
+    }
+    index++;
+    loadQuestion();
+    return;
+}
+
+const getAnswer = () => {
+    let answer;
+    optionInputs.forEach(
+        (input) => {
+            if (input.checked) {
+                answer = input.value;
+            }
+        }
+    )
+    return answer;
+}
+
+const reset = () => {
+    optionInputs.forEach(
+        (input) => {
+            input.checked = false;
+        }
+    )
+}
+
+const endQuiz = () => { 
+    document.getElementById("box").innerHTML = `
+    <div style = "text-align:center">
+    <h3>ThankYou for playing the quiz</h3>
+    <h2>${right}/${total} are correct </h2>
+    </div>
+    `
 }
 
 loadQuestion();
